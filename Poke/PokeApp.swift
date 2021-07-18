94e55ad79d8b6cfc319905cc415ff03a38046157
DylanVelezCode <dylanvelez.code@gmail.com>
2021-07-17 20:23:13 -0500

    Initial Commit

---
+++ Poke/PokeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeApp.swift
+//  Poke
+//
+//  Created by Dylan Vélez on 17/07/21.
+//
+
+import SwiftUI
+
+@main
+struct PokeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
